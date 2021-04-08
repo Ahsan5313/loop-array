ed87548f7a401f76d253e32899308faa76eb9136
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-08 12:04:19 +0600

    add html and javascript for the project

---
+++ index.js
@@ -0,0 +1,10 @@
+var nam = Array();
+  for(c=0; c<=4; c++){
+      nam = parseFloat(prompt("Enter number"));
+  }
+    sum=0;
+var nam =  [10, 20, 30, 40, 50] 
+    for(var c=0; c<=4; c++){
+     sum=sum+(nam[c]);
+    }
+document.write(sum);
